e892a8fb7960445f675e0c861a640666ff76bf52
Diamond Deltahedron <183671108+DIAMONDDELTAHEDRON@users.noreply.github.com>
2026-05-23 06:23:27 -0700

    Update lwraineffect.lua

--- scripts/world/controllers/lwraineffect.lua
+++ scripts/world/controllers/lwraineffect.lua
@@ -4,6 +4,7 @@ function LightRainEffectLoader:onLoad()
     super.onLoad(self)
     local properties = data and data.properties or {}
 	local rainfx = Game.world:spawnObject(LightRainEffect())
+	rainfx:setLayer(WORLD_LAYERS["below_ui"])
 	rainfx.max_particles = properties["maxparticles"] or Game:getFlag("hometown_rain_max_particles", rainfx.max_particles)
 	rainfx.drop_wait = properties["dropwait"] or Game:getFlag("hometown_rain_drop_wait", rainfx.drop_wait)
 end
