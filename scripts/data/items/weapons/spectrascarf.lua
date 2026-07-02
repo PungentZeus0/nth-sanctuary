663e335487269e4419a0695c78eec9827e475bc5
name = theLucidLibrarian <alexanderhaejunkim@gmail.com>
2026-07-02 14:11:58 -0700

    Whoops

---
+++ scripts/data/items/weapons/spectrascarf.lua
@@ -0,0 +1,59 @@
+local item, super = Class(Item, "spectrascarf")
+
+function item:init()
+    super.init(self)
+
+    -- Display name
+    self.name = "SpectraScarf"
+
+    -- Item type (item, key, weapon, armor)
+    self.type = "weapon"
+    -- Item icon (for equipment)
+    self.icon = "ui/menu/icon/scarf"
+
+    -- Battle description
+    self.effect = ""
+    -- Shop description
+    self.shop = ""
+    -- Menu description
+    self.description = "A rainbow scarf formed from sound itself. \n It sounds like waves."
+
+    -- Default shop price (sell price is halved)
+    self.price = 250
+    -- Whether the item can be sold
+    self.can_sell = true
+
+    -- Consumable target mode (ally, party, enemy, enemies, or none)
+    self.target = "none"
+    -- Where this item can be used (world, battle, all, or none)
+    self.usable_in = "all"
+    -- Item this item will get turned into when consumed
+    self.result_item = nil
+    -- Will this item be instantly consumed in battles?
+    self.instant = false
+
+    -- Equip bonuses (for weapons and armor)
+    self.bonuses = {
+        attack = Game.chapter == 3 and 3 or 6,
+        defense = 2,
+        magic  = 3,
+    }
+    -- Bonus name and icon (displayed in equip menu)
+    self.bonus_name = nil
+    self.bonus_icon = nil
+
+    -- Equippable characters (default true for armors, false for weapons)
+    self.can_equip = {
+        ralsei = true,
+    }
+
+    -- Character reactions
+    self.reactions = {
+        susie = "Nah, too colorful for me.",
+        ralsei = "Synthesia? What's that?",
+        noelle = "Did it just say antonymph??",
+        jamm = "Strange scarf...",
+    }
+end
+
+return item
